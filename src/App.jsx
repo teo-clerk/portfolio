98df23afd091d689e6dd2301aa48cffec89d3255
Teo Clerk <clerici.teo5@gmail.com>
2025-12-09 16:38:50 +0100

    feat: Implement 3D Floating Lines with mouse interaction

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,16 @@ function App() {
       {/* Layer 1: Background is handled via CSS on body/app-container */}
       
       {/* Layer 2: Floating 3D Lines */}
-      <FloatingLines />
+      {/* Layer 2: Floating 3D Lines */}
+      <FloatingLines 
+        enabledWaves={['top', 'middle', 'bottom']}
+        lineCount={[10, 15, 20]}
+        lineDistance={[8, 6, 4]}
+        bendRadius={5.0}
+        bendStrength={-0.5}
+        interactive={true}
+        parallax={true}
+      />
       
       {/* Layer 3: Terminal */}
       <Terminal />
